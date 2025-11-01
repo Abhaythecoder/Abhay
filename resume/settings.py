af028e51cfe266a2f58f80feba43c3b16aaf6dc3
Abhaythecoder <88576691+Abhaythecoder@users.noreply.github.com>
2025-11-01 19:20:51 +0530

    email id correction

--- resume/settings.py
+++ resume/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-=v7qy&&5-+nm^o#6_&-m)kxxx92z_b-&7h((e6yjqvq4583k*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['abhay-o8uz.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
